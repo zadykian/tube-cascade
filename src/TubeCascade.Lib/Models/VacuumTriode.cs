2e50fd7969eca4485f696364aa88ddd34b33aaf5
zadykian <zadykian@outlook.com>
2022-04-11 00:32:59 +0300

    VacuumTriode model

--- src/TubeCascade.Lib/Models/VacuumTriode.cs
+++ src/TubeCascade.Lib/Models/VacuumTriode.cs
@@ -1,17 +1,41 @@
+using System.Collections.Immutable;
 using TubeCascade.ElementList;
 using TubeCascade.Primitives;
 
 namespace TubeCascade.Models;
 
 /// <summary>
+/// Tube's anode characteristic - dependency between anode currency and anode voltage
+/// for specific voltage between tube's grid and anode.
+/// </summary>
+public readonly struct AnodeCharacteristic
+{
+	public AnodeCharacteristic(Voltage gridAnodeVoltage)
+	{
+		GridAnodeVoltage = gridAnodeVoltage;
+	}
+
+	/// <summary>
+	/// Voltage between tube's grid and anode.
+	/// </summary>
+	public Voltage GridAnodeVoltage { get; }
+	
+	// todo
+}
+
+/// <summary>
 /// Vacuum tube triode.
 /// </summary>
 public readonly struct VacuumTriode
 {
-	public VacuumTriode(Voltage nominalVoltage, Resistance internalResistance)
+	public VacuumTriode(
+		Voltage nominalVoltage,
+		Resistance internalResistance,
+		IEnumerable<AnodeCharacteristic> anodeCharacteristics)
 	{
 		NominalVoltage = nominalVoltage;
 		InternalResistance = internalResistance;
+		AnodeCharacteristics = anodeCharacteristics.ToImmutableArray();
 	}
 
 	/// <summary>
@@ -23,4 +47,9 @@ public readonly struct VacuumTriode
 	/// Tube's internal resistance.
 	/// </summary>
 	public Resistance InternalResistance { get; }
+
+	/// <summary>
+	/// Current-voltage characteristics of tube.
+	/// </summary>
+	public IReadOnlyCollection<AnodeCharacteristic> AnodeCharacteristics { get; }
 }
